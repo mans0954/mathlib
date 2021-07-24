426510e1745a94133e6acbf68a9226dbf3a0da80
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2021-07-24 22:46:54 +0200

    computing Hausdorff measures

--- src/measure_theory/hausdorff_vector_space.lean
+++ src/measure_theory/hausdorff_vector_space.lean
@@ -10,3 +10,32 @@ import measure_theory.lebesgue_measure
 # Haudorff measure in vector spaces
 
 -/
+
+open set
+open_locale measure_theory ennreal
+
+variables {ι : Type*} [fintype ι] [nonempty ι]
+
+lemma zoug : μH[fintype.card ι] (set.pi (univ : set ι) (λ i, Ico (0 : ℝ) 1)) ≤ 1 :=
+begin
+  have N : ℕ := sorry,
+  have N_pos : 0 < N := sorry,
+  have N_pos' : (0 : ℝ) < N := by exact_mod_cast N_pos,
+  let t : (ι → fin N) → set (ι → ℝ) := λ a, set.pi univ (λ i, Ico (a i/N) ((a i + 1)/N)),
+  have : set.pi (univ : set ι) (λ i, Ico (0 : ℝ) 1) ⊆ ⋃ (a : ι → fin N), t a,
+  { assume c hc,
+    simp only [mem_univ_pi, mem_Ico] at hc,
+    let d : ι → ℕ := λ i, nat_floor (c i * N),
+    have d_lt : ∀ i, d i < N,
+    { assume i,
+      rw [nat_floor_lt_iff (mul_nonneg (hc i).1 (nat.cast_nonneg _))],
+      conv_rhs { rw ← one_mul (N : ℝ) },
+      exact mul_lt_mul (hc i).2 le_rfl N_pos' zero_le_one },
+    refine mem_Union.2 ⟨λ i, ⟨d i, d_lt i⟩, _⟩,
+    simp only [mem_univ_pi, mem_Ico, fin.coe_mk, coe_coe],
+    refine λ i, ⟨_, _⟩,
+    { rw div_le_iff N_pos',
+      exact nat_floor_le (mul_nonneg (hc i).1 (nat.cast_nonneg _)) },
+    { rw lt_div_iff N_pos',
+      exact lt_nat_floor_add_one _ } },
+end
