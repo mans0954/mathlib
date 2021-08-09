69dba2920cdceb5a8fe86e942f0329ea9487888f
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-08-08 23:47:05 -0400

    fix

--- src/linear_algebra/special_linear_group.lean
+++ src/linear_algebra/special_linear_group.lean
@@ -72,11 +72,6 @@ instance has_coe_to_matrix : has_coe (special_linear_group n R) (matrix n n R) :
 
 local prefix `↑ₘ`:1024 := @coe _ (matrix n n R) _
 
--- no such lemma
-@[simp] lemma coe_matrix_apply (g : special_linear_group n R) (i : n) :
-  ↑ₘg i = (g : n → n → R) i :=
-rfl
-
 lemma ext_iff (A B : special_linear_group n R) : A = B ↔ (∀ i j, ↑ₘA i j = ↑ₘB i j) :=
 iff.trans subtype.ext_iff_val ⟨(λ h i j, congr_fun (congr_fun h i) j), matrix.ext⟩
 
@@ -98,24 +93,20 @@ section coe_lemmas
 
 variables (A B : special_linear_group n R)
 
--- in our branch,
--- @[simp] lemma coe_inv : ↑(A⁻¹) = adjugate (@coe _ (matrix n n R) _ A) := rfl
 @[simp] lemma coe_inv : ↑ₘ(A⁻¹) = adjugate A := rfl
 
 @[simp] lemma coe_mul : ↑ₘ(A * B) = ↑ₘA ⬝ ↑ₘB := rfl
 
-@[simp] lemma coe_one : ↑ₘ(1 : special_linear_group n R) = (1 : matrix n n R) := rfl
+@[simp] lemma coe_one : ↑(1 : special_linear_group n R) = (1 : matrix n n R) := rfl
 
--- in our branch,
--- @[simp] lemma det_coe_matrix : det (@coe _ (matrix n n R) _ A) = 1 := A.2
 @[simp] lemma det_coe : det ↑ₘA = 1 := A.2
 
 lemma det_ne_zero [nontrivial R] (g : special_linear_group n R) :
-  det g ≠ 0 :=
-by { rw g.det_coe_fun, norm_num }
+  det ↑ₘg ≠ 0 :=
+by { rw g.det_coe, norm_num }
 
 lemma row_ne_zero [nontrivial R] (g : special_linear_group n R) (i : n):
-  g i ≠ 0 :=
+  ↑ₘg i ≠ 0 :=
 λ h, g.det_ne_zero $ det_eq_zero_of_row_eq_zero i $ by simp [h]
 
 end coe_lemmas
@@ -131,8 +122,8 @@ instance : group (special_linear_group n R) :=
 /-- `matrix.to_lin' A` is a linear equivalence on the special linear group. -/
 def to_lin' (A : special_linear_group n R) : (n → R) ≃ₗ[R] (n → R) :=
 linear_equiv.of_linear
-  (matrix.to_lin' ↑ₘA)
-  (matrix.to_lin' ↑ₘ(A⁻¹))
+  (matrix.to_lin' ↑A)
+  (matrix.to_lin' ↑(A⁻¹))
   (by rw [←to_lin'_mul, ←coe_mul, mul_right_inv, coe_one, to_lin'_one])
   (by rw [←to_lin'_mul, ←coe_mul, mul_left_inv, coe_one, to_lin'_one])
 
@@ -145,12 +136,11 @@ lemma to_lin'_mul (A B : special_linear_group n R) :
   (A * B).to_lin' = B.to_lin'.trans A.to_lin' :=
 linear_equiv.to_linear_map_injective $ matrix.to_lin'_mul A B
 
-end coe_lemmas
 lemma to_lin'_apply (A : special_linear_group n R) (v : n → R) :
-  A.to_lin' v = matrix.to_lin' ↑ₘA v := rfl
+  A.to_lin' v = matrix.to_lin' ↑A v := rfl
 
 lemma to_lin'_symm_apply (A : special_linear_group n R) (v : n → R) :
-  A.to_lin'.symm v = matrix.to_lin' ↑ₘ(A⁻¹) v := rfl
+  A.to_lin'.symm v = matrix.to_lin' ↑(A⁻¹) v := rfl
 
 /-- `matrix.special_linear_group.to_lin'` as a `monoid_hom`. -/
 @[simps]
@@ -163,36 +153,18 @@ def to_GL : special_linear_group n R →* general_linear_group R (n → R) :=
 
 lemma coe_to_GL (A : special_linear_group n R) : ↑A.to_GL = A.to_lin'.to_linear_map := rfl
 
--- this section should be last to ensure we do not use it in lemmas
-section coe_fn_instance
-
-/-- This instance is here for convenience, but is not the simp-normal form. -/
-instance : has_coe_to_fun (special_linear_group n R) :=
-{ F   := λ _, n → n → R,
-  coe := λ A, A.val }
-
-@[simp]
-lemma coe_fn_eq_coe (s : special_linear_group n R) : ⇑s = (s : matrix n n R) := rfl
-
-end coe_fn_instance
-
 variables {S : Type*} [comm_ring S]
 
 /-- A ring homomorphism from `R` to `S` induces a group homomorphism from
 `special_linear_group n R` to `special_linear_group n S`. -/
 def map (f : R →+* S) : monoid_hom (special_linear_group n R) (special_linear_group n S) :=
-{ to_fun := λ g, ⟨f.map_matrix g, ring_hom.map_det_one f g.2⟩,
-  map_one' := by simpa,
-  map_mul' := λ x y, by simpa }
+{ to_fun := λ g, ⟨f.map_matrix ↑g, ring_hom.map_det_one f g.2⟩,
+  map_one' := sorry, --by simpa,
+  map_mul' := λ x y, sorry } --by simpa }
 
 @[simp] lemma coe_matrix_map (f : R →+* S) (g : special_linear_group n R) :
-  @coe _ (matrix n n S) _ (map f g) = f.map_matrix g :=
+  (map f g : matrix n n S) = f.map_matrix ↑g :=
 rfl
-
-@[simp] lemma coe_fun_map (f : R →+* S) (g : special_linear_group n R) :
-  ⇑(map f g) = f.map_matrix g :=
-rfl
- --, ring_hom.map_det_one f (det_coe_matrix g)⟩ :=
 
 section matrix_notation
 
@@ -200,14 +172,14 @@ variables {m : ℕ}
 
 lemma cons_apply_zero
   (M : matrix (fin m) (fin m.succ) R) (v : (fin m.succ) → R) (hM : det (vec_cons v M) = 1) :
-  (@coe_fn _ special_linear_group.coe_fun (⟨vec_cons v M, hM⟩ :
+  (@has_coe.coe _ _ special_linear_group.has_coe_to_matrix (⟨vec_cons v M, hM⟩ :
     special_linear_group (fin m.succ) R)) 0 = v :=
 rfl
 
 lemma cons_apply_one
   (M : matrix (fin m.succ) (fin m.succ.succ) R) (v : (fin m.succ.succ) → R)
   (hM : det (vec_cons v M) = 1) :
-  (@coe_fn _ special_linear_group.coe_fun (⟨vec_cons v M, hM⟩ :
+  (@has_coe.coe _ _ special_linear_group.has_coe_to_matrix (⟨vec_cons v M, hM⟩ :
     special_linear_group (fin m.succ.succ) R)) 1 = M 0 :=
 cons_val_one v M
 
@@ -220,14 +192,9 @@ instance : has_coe (special_linear_group n ℤ) (special_linear_group n R) :=
 ⟨λ x, map (int.cast_ring_hom R) x⟩
 
 @[simp] lemma coe_matrix_coe (g : special_linear_group n ℤ) :
-  @coe _ (matrix n n R) _ (@coe _ (special_linear_group n R) _ g)
-  = (@coe _ (matrix n n ℤ) _ g).map (int.cast_ring_hom R) :=
+  ↑(g : special_linear_group n R)
+  = (↑g : matrix n n ℤ).map (int.cast_ring_hom R) :=
 coe_matrix_map (int.cast_ring_hom R) g
-
-@[simp] lemma coe_fun_coe (g : special_linear_group n ℤ) :
-  ⇑(@coe _ (special_linear_group n R) _ g)
-  = (@coe _ (matrix n n ℤ) _ g).map (int.cast_ring_hom R) :=
-coe_fun_map (int.cast_ring_hom R) g
 
 end cast
 
@@ -237,31 +204,34 @@ section has_neg
 each element. -/
 instance {R : Type*} [comm_ring R] [_i : fact (even (fintype.card n))] :
   has_neg (special_linear_group n R) :=
-⟨λ g, ⟨- g.1, begin
-  convert @det_smul _ _ _ _ _ g (-1),
-  { simp,
-    refl },
-  { convert (det_coe_fun g).symm,
-    simp [nat.neg_one_pow_of_even _i.elim] }
-end⟩⟩
+⟨ λ g,
+  ⟨ -↑g, by simpa [nat.neg_one_pow_of_even _i.elim, g.det_coe] using @det_smul _ _ _ _ _ ↑ₘg (-1)⟩⟩
 
 @[simp] lemma has_neg_coe_mat {R : Type*} [comm_ring R]
   (g : (special_linear_group n R)) [_i : fact (even (fintype.card n))] :
-  @coe _ (matrix n n R) _ (- g) = - (@coe _ (matrix n n R) _ g) :=
-rfl
-
-@[simp] lemma has_neg_coe_fn {R : Type*} [comm_ring R]
-  (g : (special_linear_group n R)) [_i : fact (even (fintype.card n))] :
-  @coe_fn _ _ (- g) = - (@coe_fn _ _ g) :=
+  ↑(-g) = (-↑g : matrix n n R) :=
 rfl
 
 @[simp]
 lemma has_neg_cast {R : Type*} [comm_ring R] (g : (special_linear_group n ℤ))
   [_i : fact (even (fintype.card n))] :
-  @coe _ (special_linear_group n R) _ (- g) = - (@coe _ (special_linear_group n R) _ g) :=
-subtype.ext $ (@ring_hom.map_matrix n _ _ _ _ _ _ (int.cast_ring_hom R)).map_neg g
+  ↑(-g) = (-↑g : special_linear_group n R) :=
+subtype.ext $ (@ring_hom.map_matrix n _ _ _ _ _ _ (int.cast_ring_hom R)).map_neg ↑g
 
 end has_neg
+
+-- this section should be last to ensure we do not use it in lemmas
+-- section coe_fn_instance
+
+-- /-- This instance is here for convenience, but is not the simp-normal form. -/
+-- instance : has_coe_to_fun (special_linear_group n R) :=
+-- { F   := λ _, n → n → R,
+--   coe := λ A, A.val }
+
+-- @[simp]
+-- lemma coe_fn_eq_coe (s : special_linear_group n R) : ⇑s = (s : matrix n n R) := rfl
+
+-- end coe_fn_instance
 
 end special_linear_group
 
